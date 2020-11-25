8193f7c502e79fed70bd7b92eb4ad183c55c1b29
Alexander Ildiyarov <alexvancasper@gmail.com>
2020-11-25 23:10:09 +0300

    added form processing

--- main.go
+++ main.go
@@ -6,7 +6,15 @@ import (
     "github.com/gorilla/mux"
     "math/rand"
     "strconv"
+    // "encoding/json"
+    "html/template"
 )
+
+
+type User struct {
+    Username string
+    Password string 
+}
 
 func hello(w http.ResponseWriter, req *http.Request) {
     var help string
@@ -23,8 +31,59 @@ func hello(w http.ResponseWriter, req *http.Request) {
     fmt.Fprintf(w, help)
 }
 
-// func headers(w http.ResponseWriter, req *http.Request) {
+func user(w http.ResponseWriter, req *http.Request) {
+    // w.Header().Set("Content-Type","application/json")
 
+    for key, value := range req.Form{
+        fmt.Printf("%s = %s\n", key, value)
+    }
+
+    p := User{Username: req.FormValue("username"), Password: req.FormValue("password")}
+    // js, _ := json.Marshal(p)
+    // w.Write(js)
+
+    tmplt, err := template.ParseFiles("static/user.html")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }    
+    tmplt.Execute(w, p)
+}
+
+func signup(w http.ResponseWriter, req *http.Request) {
+    req.ParseForm()
+
+    fmt.Printf("USERNAME => %s\n", req.FormValue("username"))
+    fmt.Printf("PASSWORD => %s\n", req.FormValue("password"))
+    fmt.Printf("HIDDEN => %s\n", req.FormValue("hidden"))
+    // fmt.Println("---------------------")
+    // for key, value := range req.Form{
+    //     fmt.Printf("%s = %s\n", key, value)
+    // }
+    http.Redirect(w, req, "/user", http.StatusSeeOther)
+}
+
+func indexHTMLTemplateVariableHandler(response http.ResponseWriter, request *http.Request) {
+    // tmplt := template.New("IndexTemplated.html")       //create a new template with some name
+    tmplt, err := template.ParseFiles("static/index.html") //parse some content and generate a template, which is an internal representation
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    // p := Student{Id: 1, Name: "Aisha"} //define an instance with required field
+    tmplt.Execute(response, nil) //merge template ‘t’ with content of ‘p’
+}
+
+// func TemplatedHandler(response http.ResponseWriter, request *http.Request) {
+//     tmplt := template.New("hello template")
+//     tmplt, _ = tmplt.Parse("Top Student: {{.Id}} - {{.Name}}!")
+
+//     p := Student{Id: 1, Name: "Aisha"} //define an instance with required field
+
+//     tmplt.Execute(response, p) //merge template ‘t’ with content of ‘p’
+// }
+
+// func headers(w http.ResponseWriter, req *http.Request) {
 //     for name, headers := range req.Header {
 //         for _, h := range headers {
 //             fmt.Fprintf(w, "%v: %v\n", name, h)
@@ -66,7 +125,10 @@ func number_min_max(w http.ResponseWriter, req *http.Request) {
 func main() {
 
     r := mux.NewRouter()
-    r.HandleFunc("/", hello).Methods("GET")
+    r.HandleFunc("/hello", hello).Methods("GET")
+    r.HandleFunc("/signup", signup).Methods("POST")
+    r.HandleFunc("/user", user).Methods("GET")
+    r.HandleFunc("/", indexHTMLTemplateVariableHandler).Methods("GET")
     r.HandleFunc("/number/{number:[0-9]+}", number).Methods("GET")
     r.HandleFunc("/number/{max:[0-9]+}/{number:[0-9]+}", number_max).Methods("GET")
     r.HandleFunc("/number/{min:[0-9]+}/{max:[0-9]+}/{number:[0-9]+}", number_min_max).Methods("GET")
